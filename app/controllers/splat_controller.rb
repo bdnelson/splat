211748a3669b8c6b50ea31e55c0f1eec3ac64d67
Brian Nelson <bdnelson@bdnsolutions.com>
2014-12-05 15:38:07 -0600

    Updates

--- app/controllers/splat_controller.rb
+++ app/controllers/splat_controller.rb
@@ -1,7 +1,21 @@
 class SplatController < ApplicationController
   def create
     logger.debug "FORMAT => #{params[:format]}"
-    logger.ap params[:splat]
+
+    self.request.env.each do |header|
+      logger.warn "HEADER: #{header[0]} -> #{header[1].inspect}"
+    end
+
+    params.each do |key, value|
+      logger.debug "PARAM: #{key} => #{value.inspect}"
+    end
+
+    logger.debug "BODY => #{request.body.inspect}"
+
+    body = request.body.read
+
+    logger.debug "BODY SIZE = #{body.length} bytes"
+
     head :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Splat::Application.routes.draw do
+  match 'splat' => 'splat#create', :via => :get
+  match 'bang' => 'bang#create', :via => :get
   match 'splat' => 'splat#create', :via => :post
   match 'bang' => 'bang#create', :via => :post
+  match 'splat' => 'splat#create', :via => :put
+  match 'bang' => 'bang#create', :via => :put
 end
